38e8a942dad393406d5811f2d3c8147d6501b1aa
luuizeduuardo <143219181+luuizeduuardo@users.noreply.github.com>
2025-07-01 16:10:37 -0300

    Ajuste do PedidoService para método de validaTotalPedido

--- src/main/java/br/com/bellaface/vendas/service/pedido/PedidoService.java
+++ src/main/java/br/com/bellaface/vendas/service/pedido/PedidoService.java
@@ -2,11 +2,28 @@ package br.com.bellaface.vendas.service.pedido;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.bellaface.vendas.model.itemPedido.ItemPedido;
+import br.com.bellaface.vendas.repository.itemPedido.ItemPedidoRepository;
 
 @Service
 public class PedidoService {
+	
+	public static PedidoService instance;
+	
+	public static PedidoService getInstance() {
+		if (instance == null) {
+			instance = new PedidoService();
+		}
+		return instance;
+	}
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public boolean isFinalDeSemana() {
 		LocalDate diaAtual = LocalDate.now();
@@ -19,4 +36,24 @@ public class PedidoService {
         return false;
         
     }
+	
+	public Double validaTotalPedido(int nuPedido) {
+		List<ItemPedido> itens = itemPedidoRepository.findByNuPedido(nuPedido);
+		int itensNoPedido = itens.size();
+		Double vlTotalItensNoPedido = 0.0;
+		
+		if(itens == null || itensNoPedido == 0) {
+			return 0.0;
+		}
+		
+		for (int i = 0; i < itensNoPedido; i++ ) {
+			Double vlUnitario = itens.get(i).getVlUnitario();
+			int qtdItem = itens.get(i).getQtdItem();
+			
+			Double vlTotalItem = qtdItem * vlUnitario;
+			vlTotalItensNoPedido = vlTotalItensNoPedido + vlTotalItem;
+		}
+		
+		return vlTotalItensNoPedido;
+	}
 }
